2b5c81a7c74b5a5e12f3d023b73f3da9ce91c016
gund669 <53211308+gund669@users.noreply.github.com>
2019-10-29 15:33:38 +0200

    Updates

---
+++ precache-manifest.0e346688b65758ae5a8b6a321d03ac36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4b60390582e66cec43f9c1b2906cba",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "53a2abf15b302c9dace6",
+    "url": "/weatherapp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "beeee57ff9796a0fd922",
+    "url": "/weatherapp/static/css/main.a20ed15c.chunk.css"
+  },
+  {
+    "revision": "53a2abf15b302c9dace6",
+    "url": "/weatherapp/static/js/2.34d954e5.chunk.js"
+  },
+  {
+    "revision": "beeee57ff9796a0fd922",
+    "url": "/weatherapp/static/js/main.44ccca03.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  },
+  {
+    "revision": "55d5567d5080cf9a8f294bad872ffc08",
+    "url": "/weatherapp/static/media/blur-background-1187974.55d5567d.jpg"
+  }
+]);
